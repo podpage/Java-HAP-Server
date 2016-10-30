f03f6904898ee6a83fb98c0133604b5caa419f5a
podpage <support@podpage.org>
2016-10-30 19:35:04 +0100

    Added Example and improved README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Java-HAP-Server
-HAP-Server based on beowulfe/HAP-Java
+HAP-Server based on [beowulfe/HAP-Java](https://github.com/beowulfe/HAP-Java)
+
+This project would not have been possible without the work of [Tian Zhang](https://github.com/KhaosT) and [Andy Lintner](https://github.com/beowulfe)! <3
+
+### Installation
+
+  - 1). Download
+  - 2). Start and Stop (generating config and folders)
+  - 3). Dump accessory files in "plugins" folder
+  - 4). Start
+  - 5). Connect your iPhone
+  - 6). done!
+
+
+### Plugins
+
+```java
+@LoadableAccessory
+public class MockLight implements Lightbulb {
+```
+
+Create an accessory and add the @LoadableAccessory annotation on top of the class.
+Export as .jar and put the file in the "plugins" folder.
+Easy as that
+
+
+If you need help or have any question just text me [@podpage](https://twitter.com/podpage)
---
+++ src/example/java/org.podpage.hap/MockLight.java
@@ -0,0 +1,72 @@
+package org.podpage.hap;
+
+import com.beowulfe.hap.HomekitCharacteristicChangeCallback;
+import com.beowulfe.hap.accessories.Lightbulb;
+import org.podpage.hap.accessory.annotation.LoadableAccessory;
+
+import java.util.concurrent.CompletableFuture;
+
+@LoadableAccessory
+public class MockLight implements Lightbulb {
+
+    private boolean powerState = false;
+    private HomekitCharacteristicChangeCallback subscribeCallback = null;
+
+    @Override
+    public int getId() {
+        return 2;
+    }
+
+    @Override
+    public String getLabel() {
+        return "Light";
+    }
+
+    @Override
+    public void identify() {
+        System.out.println("Identifying light");
+    }
+
+    @Override
+    public String getSerialNumber() {
+        return "none";
+    }
+
+    @Override
+    public String getModel() {
+        return "none";
+    }
+
+    @Override
+    public String getManufacturer() {
+        return "none";
+    }
+
+    @Override
+    public CompletableFuture<Boolean> getLightbulbPowerState() {
+        return CompletableFuture.completedFuture(powerState);
+    }
+
+    @Override
+    public CompletableFuture<Void> setLightbulbPowerState(boolean powerState)
+            throws Exception {
+        this.powerState = powerState;
+        if (subscribeCallback != null) {
+            subscribeCallback.changed();
+        }
+        System.out.println("The Light is " + powerState ? "on" : "off");
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Override
+    public void subscribeLightbulbPowerState(
+            HomekitCharacteristicChangeCallback callback) {
+        this.subscribeCallback = callback;
+    }
+
+    @Override
+    public void unsubscribeLightbulbPowerState() {
+        this.subscribeCallback = null;
+    }
+
+}
